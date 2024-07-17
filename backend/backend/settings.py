056c9e5af641e1580b67f3ed76f1f22b90cc795c
sravyavulisetty <sravyavulisetty269@gmail.com>
2024-07-17 18:19:12 -0400

    changes made to corsheaders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOWED_ORIGINS = (
+    u'http://localhost:3000',
+    u'http://184.72.115.206:8000'
+)
